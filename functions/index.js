8be103403d71c739b4311699eca407a51ce13a7e
fhernan22 <fhern103@fiu.edu>
2021-02-09 23:25:56 -0500

    Testing

--- functions/index.js
+++ functions/index.js
@@ -5,5 +5,5 @@ const functions = require("firebase-functions");
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello Firebase!");
+  response.send("Hello World!");
 });
