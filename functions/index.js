e395c897d0ecc62021019ce58e74eb5f702480a8
papijuancho <78453905+papijuancho@users.noreply.github.com>
2021-02-11 19:01:15 -0500

    Testing

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-// testing
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello World!");
