50dfdd267b5bb252bfec109aa898a23e34104625
fhernan22 <fhern103@fiu.edu>
2021-03-05 19:36:57 -0500

    Deleted index.test.js

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,29 +1,26 @@
+// This test should be deleted. /helloworld doesn't exist anymore
 
 // var first = require("../Index");
-const request = require("supertest");
+// const request = require("supertest");
 
+// describe("Cloud Functions", () => {
+//     let myFunctions;
 
+//     before(() => {
+//       myFunctions = require("../index");
+//     });
 
-describe("Cloud Functions", () => {
-    let myFunctions;
-  
-    before(() => {
-      myFunctions = require("../index");
-    });
-  
-    describe("GET /helloworld", () => {
-      it("It should return Hello World! May the force be with you!", (done) => {
-        request(myFunctions.api)
-          .get("/helloWorld")
-          .expect("Hello World! May the force be with you!")
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-  
-            return done();
-          });
-      });
-    });
-  });
-  
-  
+//     describe("GET /helloworld", () => {
+//       it("It should return Hello World! May the force be with you!", (done) => {
+//         request(myFunctions.api)
+//           .get("/helloWorld")
+//           .expect("Hello World! May the force be with you!")
+//           .expect(200)
+//           .end((err, res) => {
+//             if (err) return done(err);
+
+//             return done();
+//           });
+//       });
+//     });
+//   });
